4558169ea5ae291cfaf9536262285394850e1d53
Marcela <mgonzalez@puntoticket.cl>
2026-09-18 07:43:40 -0300

    feat: implement redis cache service

---
+++ RedisCacheDemo.Application/Abstractions/Caching/ICacheService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCacheDemo.Application.Abstractions.Caching
+{
+    public interface ICacheService
+    {
+        Task<T?> GetAsync<T>(string key);
+
+        Task SetAsync<T>(
+            string key,
+            T value,
+            TimeSpan? expiration = null);
+
+        Task RemoveAsync(string key);
+    }
+}
---
+++ RedisCacheDemo.Infrastructure/Caching/RedisCacheService.cs
@@ -0,0 +1,27 @@
+﻿using RedisCacheDemo.Application.Abstractions.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCacheDemo.Infrastructure.Caching
+{
+    public class RedisCacheService : ICacheService
+    {
+        public Task<T?> GetAsync<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync<T>(string key, T value, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
