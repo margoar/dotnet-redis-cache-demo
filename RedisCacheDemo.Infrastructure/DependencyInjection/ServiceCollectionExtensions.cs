a86e2e7e7273985c2e0bcdd7bcef73c24a9944a5
Marcela <mgonzalez@puntoticket.cl>
2026-09-18 08:05:18 -0300

    feat: add redis configuration

---
+++ RedisCacheDemo.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
+using RedisCacheDemo.Infrastructure.Configuration;
+
+namespace RedisCacheDemo.Infrastructure.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<RedisOptions>(
+            configuration.GetSection(RedisOptions.SectionName));
+
+        services.AddSingleton<IConnectionMultiplexer>(serviceProvider =>
+        {
+            var redisOptions = serviceProvider
+                .GetRequiredService<IOptions<RedisOptions>>()
+                .Value;
+
+            return ConnectionMultiplexer.Connect(
+                redisOptions.ConnectionString);
+        });
+
+        return services;
+    }
+}
